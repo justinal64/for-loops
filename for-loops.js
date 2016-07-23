0d07d6eaae710b71b419215654cb649f3768fa84
justin leggett <justin@codingrookie.com>
2016-07-23 13:45:51 -0500

    modify tasks 1 and 2 are complete.

--- for-loops.js
+++ for-loops.js
@@ -1,1 +1,20 @@
-console.log("Test");
+// Task 1
+for (var i = 5; i < 120;) {
+    // console.log(i);
+    i = i + 10;
+}
+
+// Task 2
+for (var j = 4096; j >= 1;) {
+    // console.log(j);
+    j = j / 2;
+}
+
+// Task 3
+var pres = ["George Washington", "John Adams", "Thomas Jefferson"];
+
+console.log(pres.length);
+
+// for (var k = 0; k < pres.length; i++) {
+//     console.log("President #" + [i] + " was " +  pres[i]);
+// }
